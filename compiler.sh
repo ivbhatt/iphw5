ce0cbf62d60ff5792709c43a5b2f29a0402f8a58
ivbhatt <ivbhatt@ncsu.edu>
2022-03-03 03:08:59 -0500

    bug fix

--- compiler.sh
+++ compiler.sh
@@ -1,3 +1,5 @@
+mkdir executables
+
 gcc code/tcp-client.c -o executables/tcp-client-c > /dev/null
 gcc code/tcp-concurrent-server.c -o executables/tcp-concurrent-server-c > /dev/null
 gcc code/tcp-iterative-server.c -o executables/tcp-iterative-server-c > /dev/null
