de561ac54d0edfa9ff82ae09f09a32e73aa288f4
ivbhatt <ivbhatt@ncsu.edu>
2022-03-03 03:07:16 -0500

    ignoring executables

--- compiler.sh
+++ compiler.sh
@@ -1,13 +1,14 @@
-gcc -c code/tcp-client.c -o executables/tcp-client-c > /dev/null
-gcc -c code/tcp-concurrent-server.c -o executables/tcp-concurrent-server-c > /dev/null
-gcc -c code/tcp-iterative-server.c -o executables/tcp-iterative-server-c > /dev/null
+gcc code/tcp-client.c -o executables/tcp-client-c > /dev/null
+gcc code/tcp-concurrent-server.c -o executables/tcp-concurrent-server-c > /dev/null
+gcc code/tcp-iterative-server.c -o executables/tcp-iterative-server-c > /dev/null
 
-gcc -c code/udp-client.c -o executables/udp-client-c > /dev/null
-gcc -c code/udp-server.c -o executables/udp-server-c > /dev/null
+gcc code/udp-client.c -o executables/udp-client-c > /dev/null
+gcc code/udp-server.c -o executables/udp-server-c > /dev/null
 
-javac code/tcp-client.java -d executables
-javac code/tcp-server.java -d executables
+javac code/tcp-client.java -d executables > /dev/null
+javac code/tcp-server.java -d executables > /dev/null
 
-javac code/udp-client.java -d executables
-javac code/udp-server.java -d executables
+javac code/udp-client.java -d executables > /dev/null
+javac code/udp-server.java -d executables > /dev/null
 
+chmod 777 executables/*
